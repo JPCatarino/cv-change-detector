3f0722b934425c122963b64fcef03d0936ab12f8
JPCatarino <jorge.catarino@ua.pt>
2021-01-09 16:33:16 +0000

    [DET] Change content of __init__to main, add requirements

--- __init__.py
+++ __init__.py
@@ -1,13 +1,1 @@
-# Python Change Detector using Open CV
 
-import cv2
-
-import numpy as np
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
---
+++ main.py
@@ -0,0 +1,15 @@
+# Python Change Detector using Open CV
+
+import cv2
+
+import numpy as np
+
+from utils import image_processing
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
